504e2ff517504e17df377397e0ec1cdc0a504565
Y Paul Sussman <ypaulsussman@gmail.com>
2020-07-20 20:51:23 -0500

    Revert "add filetype to troubleshoot netlify"
    This reverts commit 600fa156c6575e5c7e2c39484f60d19c922b77be.

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import Demo from "../pages/demo/Demo.js";
-import CharGen from "../pages/CharGen/CharGen.js";
+import Demo from "../pages/demo/Demo";
+import CharGen from "../pages/CharGen/CharGen";
 import "./App.scss";
 
 const App = () => {
