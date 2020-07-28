519ba60dd31d63e231012bdcac2cf16faa2bf2c6
Y Paul Sussman <ypaulsussman@gmail.com>
2020-07-28 18:10:26 -0500

    cosmetic/content changes

--- src/pages/lander/Lander.js
+++ src/pages/lander/Lander.js
@@ -2,14 +2,39 @@ import React from "react";
 // react router link for chargen / mapquiz
 // add header component
 
-
 const Lander = () => {
-  return(
-  <>
-  <h1>sup</h1>
-  </>
-  )
-}
+  return (
+    <main>
+      <h1>Welcome to the Campaign (setup)!</h1>
 
+      <section>
+        <p>If you're here, then either:</p>
+        <ol>
+          <li>
+            I sent you a token so we could get you onboarded and prepped to
+            play, and/or
+          </li>
+          <li>
+            ...you know how to use browser DevTools (
+            <em>which is also cool!</em>)
+          </li>
+        </ol>
+        <p>
+          So let's get started! The backgrounder map-n'-quiz should take ~10
+          min, while you can spend as much time as you like on the rando-PC
+          generator.
+        </p>
+      </section>
+      <section>
+        <h2>Backgrounder Map and Quiz</h2>
+        <p>more text here -- why you created it, and what we'll use it for</p>
+      </section>
+      <section>
+        <h2>Random-PC Generator</h2>
+        <p>more text here -- why you created it, and what we'll use it for</p>
+      </section>
+    </main>
+  );
+};
 
 export default Lander;
